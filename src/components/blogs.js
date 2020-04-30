ac98516d791e904a2d10a951c0bcfa85726b846e
Jonathan Moss <nathaniel.coldfyre@gmail.com>
2020-04-30 14:20:51 -0700

    added mailchimp

--- src/components/blogs.js
+++ src/components/blogs.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Img from "gatsby-image";
 import { Link } from "gatsby";
-import moment from "moment";
 
 export default class Blogs extends Component {
   render() {
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Mailchimp from "react-mailchimp-form";
 
 export default class Contact extends Component {
   render() {
@@ -6,7 +7,21 @@ export default class Contact extends Component {
       <div className="contact section" id="Contact">
         <div className="container">
           <div className="section-head">
-            <h2 className="text-center">Contact applejacksranch@gmail.com</h2>
+            <h2 className="text-center">Sign up for our newsletter!</h2>
+            <Mailchimp
+              action="https://app.us8.list-manage.com/subscribe/post?u=f2639e87a7aa751a8adae9713&amp;id=8aa423b577"
+              fields={[
+                {
+                  name: "EMAIL",
+                  placeholder: "Email",
+                  type: "email",
+                  required: true,
+                },
+              ]}
+            />
+            <h2 className="text-center">
+              Or, just contact us at applejacksranch@gmail.com
+            </h2>
           </div>
           <form
             action={`https://formspree.io/maydrwrn`}
