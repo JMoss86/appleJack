fdb8bfe6b4586234eb2ceae8be39240c57b16963
Jonathan Moss <jdmoss86@gmail.com>
2021-08-10 18:19:05 -0700

    added donate button

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,6 +6,19 @@ export default class Contact extends Component {
     return (
       <div className="contact section" id="Contact">
         <div className="container">
+          <div className="section-donate">
+            <h2 className="text-center">Donate to Make a Difference!</h2>            
+            <div>
+              <button type="submit">
+                <form action="https://www.paypal.com/donate" method="post" target="_top">
+                  <input type="hidden" name="hosted_button_id" value="96A3TNTP5TEN8" />
+                  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                  <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+              </button>
+            </div>
+            <br />            
+          </div>
           <div className="section-head">
             <h2 className="text-center">Sign up for our newsletter</h2>
             <Mailchimp
